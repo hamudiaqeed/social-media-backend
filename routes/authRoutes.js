81a43339ed4f388e090c924ea87cf7e86f005c60
Mohammed Aqeed <hamudy_fun5b@yahoo.com>
2023-01-25 14:43:55 +0200

    Delete authRoutes.js

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const authController = require('../controllers/authController');
-
-router.put('/signup', authController.signup);
-
-router.post('/login', authController.login);
-
-module.exports = router;
